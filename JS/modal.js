3ca07028cb1cfa206e500f4a8e41f286c5f2556f
Vladyslav Yevtikhevych <evtik2.0@gmail.com>
2026-02-21 18:14:10 +0100

    Add files via upload
    Successfully finalized the bakery website, delivering a fully responsive design for mobile and desktop. Completed layout implementation, styling, and core functionality development.

---
+++ JS/modal.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const modal = document.querySelector('.backdrop');
+  const modalBtnsOpen = document.querySelectorAll('.modal-btn-open');
+  const modalBtnClose = document.querySelector('.modal-btn-close');
+
+  const toggleModal = () => modal.classList.toggle('is-hidden');
+
+  modalBtnsOpen.forEach(btn => {
+    btn.addEventListener('click', toggleModal);
+  });
+
+  modalBtnClose?.addEventListener('click', toggleModal);
+});
